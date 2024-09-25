9b06da04192b1bdbdfaf1eab10e4dd6c1a3191fe
Ajay Dangi <128664911+ajay8962@users.noreply.github.com>
2024-09-25 09:37:57 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-
+import CV from "../../assets/ajaycv.jpg";
 import Info from "./Info.jsx";
 export default function About() {
     return(
